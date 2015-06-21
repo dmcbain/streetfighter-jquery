9e8abdb89d1611572ebc7cea8450d718b7d927f5
dmcbain <dwainmcbain@hotmail.com>
2015-06-21 21:16:03 +0100

    added audio

--- js/app.js
+++ js/app.js
@@ -9,10 +9,18 @@ $(document).ready(function() {
 })
 
 .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+	$('.hadouken').finish().show()
+    .animate(
+    	{'left': '1020px'},
+  500,
+  function() {
+    $(this).hide();
+    $(this).css('left', '520px');
+  }
+  );
     // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
@@ -25,3 +33,9 @@ $(document).ready(function() {
   });
 
 });
+// sound function
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
